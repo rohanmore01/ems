f9a6cf13e75e0afb35295e0696b4d6c992639e41
rohanmore01 <morerohan11007@gmail.com>
2024-06-01 11:21:56 +0530

    added ENV variable

--- db.php
+++ db.php
@@ -1,10 +1,14 @@
 <?php
-$serverName = 'mysql';
-$userName = 'root';
-$password = 'Nic@123';
-$dbName = "ems";
-$conn = mysqli_connect($serverName, $userName, $password, $dbName);
 
-if (!$conn) {
-    die('Cannot connect to mysql Database' . mysql_error());
+define("DB_HOST", $_ENV["MYSQL_DATABASE_SERVICE"]);
+define("DB_USER", $_ENV["MYSQL_USER"]);
+define("DB_PASSWORD", $_ENV["MYSQL_PASSWORD"]);
+define("DB_NAME", $_ENV["MYSQL_DATABASE"]);
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
+
+$conn->select_db(DB_NAME);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
